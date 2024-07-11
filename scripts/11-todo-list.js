9248b7307ac294a657632943a437a725a37eebb1
alitsayyed <alitsayyed@gmail.com>
2024-07-11 16:21:45 -0500

    Todo List Completed

---
+++ scripts/11-todo-list.js
@@ -0,0 +1,39 @@
+const todoList = [{
+  name: 'make dinner',
+  dueDate:'2024-7-11'}, {name: 'wash dishes', dueDate: '2024-7-22'}];
+
+renderTodoList(); // render page every time it is loaded
+
+function renderTodoList(){
+  let todoListHTML = '';
+  for (let i = 0; i < todoList.length; i++){
+    const todoObject = todoList[i];  
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
+    const { name, dueDate } = todoObject;
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+     <button onclick ="
+      todoList.splice(${i},1);
+      renderTodoList();
+     " class="delete-todo-button">Delete</button>
+    `;
+    todoListHTML += html;
+  }
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+function addTodo(){
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value // grabs value inside the input field and stores it 
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({name: name, dueDate: dueDate});
+
+  inputElement.value = ''; // makes the value in the text box empty again. 
+  renderTodoList(); // update page after adding a todo.
+}
